0e74c5098f52c3b0ba4a0989371e2e4813cc534e
Nima Habibollahi <n.habibollahi@yahoo.com>
2024-02-04 14:52:02 +0330

    Product Web API Controller

---
+++ ProductsEF/Controllers/ProductsController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductsEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        ProductDbContext dbContext;
+
+       public ProductsController(ProductDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var products=dbContext.Products.Select(p => (p.Name+p.Description+p.Price)).ToList();
+
+            return products ;
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromBody] Product product)
+        {
+            dbContext.Products.Add(product);
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = product.id }, product);
+        }
+        bool productExists(long id)
+        {
+            return dbContext.Products.Any(e => e.id == id);
+        }
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] long id, Product product)
+        {
+            if (id != product.id)
+            {
+                return  BadRequest();
+            }
+
+            dbContext.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!productExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(long id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Products.Remove(product);
+            await dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
